cd98052d64a6138bca790f5cab2b8db3e0c5bed3
zmcentire <mcentirezach@gmail.com>
2021-04-08 19:09:44 -0600

    comment form

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,3 +1,5 @@
+import React, { useRef } from 'react';
+
 import Header from './header';
 import Image from './image';
 import Actions from './actions';
@@ -5,9 +7,27 @@ import Footer from './footer';
 import Comments from './comments';
 
 const Post = ({ content }) => {
+    const commentInput = useRef(null);
+
+    const handleFocus = () => commentInput.current.focus();
+
     return (
-        <div>
-            <p>I am a post</p>
+        <div className="rounded col-span-4 border bg-white mb-16">
+            <Header username={content.username} />
+            <Image src={content.imageSrc} caption={content.caption} />
+            <Actions 
+                docId={content.docId}
+                totalLikes={content.likes.length}
+                likedPhoto={content.userLikedPhoto}
+                handleFocus={handleFocus}
+            />
+            <Footer username={content.username} caption={content.caption} />
+            <Comments
+                docId={content.docId}
+                comments={content.comments}
+                posted={content.dateCreated}
+                commentInput={commentInput}
+            />
         </div>
     )
 }
