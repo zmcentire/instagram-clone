1dab60e7c255b137a95672f154d2a87191866c56
zmcentire <mcentirezach@gmail.com>
2021-04-27 18:28:40 -0600

    user profile state

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,5 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import Header from './header';
+import Photos from './photos';
+import { getUserByUsername, getUserPhotosByUsername } from '../../services/firebase';
+
+const reducer = (state, newState) => ({ ...state, ...newState});
+const initialState = {
+    profile: {},
+    photosCollections: [],
+    followerCount: 0
+}
 
 export default function Profile({ username }) {
-    
+    const [{ profile, photosCollections, followerCount}, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
+
+    useEffect(() => {
+        async function getProfileInfoAndPhotos() {
+            const [{ ...user }] = await getUserByUsername(username);
+            const photos = await getUserPhotosByUsername(username);
+
+            dispatch({ profile: user, photosCollection: photos, followerCount:
+            user.followers.length });
+        }
+        getProfileInfoAndPhotos();
+    }, [username]);
+
+    return (
+        <>
+            <Header 
+                photosCollection={photosCollection.length}
+                profile={profile}
+                followerCount={followerCount}
+                setFollowerCount={dispatch}
+            />
+            <Photos photos={photosCollection}/>
+        </>
+    )
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -86,5 +86,46 @@ export async function updateFollowedUserFollowers(docId, followingUserId, isFoll
 }
 
 export async function getUserByUsername() {
-    
+    const result = await firebase
+        .firestore()
+        .collection('users')
+        .where('username', '==', username)
+        .get();
+
+    const user = result.docs.map((item) => ({
+        ...item.data(),
+        docId: item.id
+    }));
+
+    return user.length > 0 ? user : false;
+}
+
+export async function getUserIdByUsername(username) {
+    const result = await firebase
+        .firestore()
+        .collection('users')
+        .where('username', '==', username)
+        .get();
+        
+    const [{ userId = null }] = result.docs.map((item) => ({
+        ...item.data(),
+    }));
+
+    return userId;
+}
+
+export async function getUserPhotosByUsername() {
+    const userId = await getUserIdByUsername(username);
+    const result = await firebase   
+        .firestore()
+        .collection('photos')
+        .where('userId', '==', userId)
+        .get();
+
+    const photos = result.docs.map((item) => ({
+        ...item.data(),
+        docId: item.id
+    }))
+
+    return photos;
 }
